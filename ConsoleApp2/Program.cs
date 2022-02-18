d5398ab09223969abd700d970e7362f25f57bac8
ALUNO <ALUNO@DESKTOP-9HNBEJR>
2022-02-18 15:38:30 -0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o número de comandos emitidos pelo SGT: ");
+            double num = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite a série de comandos emitidos pelo SGT, utilizando 'E' para esquerda e 'D' para direita: ");
+            string comandos = Convert.ToString(Console.ReadLine().ToUpper());
+            char inicial = 'N';
+
+            foreach (char comando in comandos)
+            {
+
+                if (comando == 'D')
+                {
+                    if (inicial == 'N')
+                    {
+                        inicial = 'L';
+                    }
+                    else if (inicial == 'L')
+                    {
+                        inicial = 'S';
+                    }
+                    else if (inicial == 'S')
+                    {
+                        inicial = 'O';
+                    }
+                    else if (inicial == 'O')
+                    {
+                        inicial = 'N';
+                    }
+                }
+
+                else if (comando == 'E')
+                {
+                    if (inicial == 'N')
+                    {
+                        inicial = 'O';
+                    }
+                    else if (inicial == 'O')
+                    {
+                        inicial = 'S';
+                    }
+                    else if (inicial == 'S')
+                    {
+                        inicial = 'L';
+                    }
+                    else if (inicial == 'L')
+                    {
+                        inicial = 'N';
+                    }
+
+                }
+            }
+            Console.WriteLine($"a direção para qual o soldado está olhando é: {inicial}");
+
+        }
+    }
+}
---
+++ freq de numeros/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace freq_de_numeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a quantidade de valores a serem lidos: ");
+            int qtd = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[qtd];
+            while (qtd >= 1)
+            {
+                qtd--;
+                Console.Write("Digite os valores: ");
+                int valores = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+            
+
+
+
+
+
+
+
+        }
+    }
+}
